34f531eb8fee4806a4b8a224acc20fa11c71f2c0
Dimezis <Dmitry.Savyuk@gmail.com>
2016-05-03 22:54:25 +0300

    Naming changes

--- app/src/main/kotlin/rxbus/example/MainActivity.kt
+++ app/src/main/kotlin/rxbus/example/MainActivity.kt
@@ -19,30 +19,30 @@ class MainActivity : BaseActivity() {
 
     private fun setClickListeners() {
         //produce example events on click
-        buttonEvent1.setOnClickListener {
-            Bus.send(Events.ExampleEvent1("ExampleEvent1 Text"))
+        buttonTextEvent.setOnClickListener {
+            Bus.send(Events.TextEvent("Example Text"))
         }
 
         var counter = 0
-        buttonEvent2.setOnClickListener {
+        buttonCounterEvent.setOnClickListener {
             counter++
-            Bus.send(Events.ExampleEvent2(count = counter))
+            Bus.send(Events.CounterEvent(count = counter))
         }
     }
 
     private fun subscribeToExampleEvents() {
-        Bus.observe<Events.ExampleEvent1>()
+        Bus.observe<Events.TextEvent>()
                 .map { "Modified ${it.title}"} //you can use any Rx operator to transform event data
                 .subscribe { textView.text = it }
                 .registerInBus(this)
 
-        Bus.observe<Events.ExampleEvent2>()
+        Bus.observe<Events.CounterEvent>()
                 .observeOn(AndroidSchedulers.mainThread()) //optional, if you need to receive event in main thread
-                .subscribe { textView.text = "ExampleEvent2 sent ${it.count} times" }
+                .subscribe { textView.text = "Counter event sent ${it.count} times" }
                 .registerInBus(this)
 
         //if you want to manage each of your subscriptions separately, you can store them locally
-        var notRegisteredSubscription = Bus.observe<Events.ExampleEvent1>()
+        var notRegisteredSubscription = Bus.observe<Events.TextEvent>()
                 .subscribe { textView.text = it.title }
                 //here we don't call registerInBus(this)
 
